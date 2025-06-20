f74a344f003aabfcf0df67714151dab3fac980e0
heny0424 <foo424@naver.com>
2025-06-20 15:47:32 +0900

    initial commit

---
+++ src/components/MainVisual.jsx
@@ -0,0 +1,11 @@
+function MainVisual() {
+return (
+    <section className="main_visual">
+        <div className="inner">
+            <img src="/images/main_visual.jpg" alt="" />
+        </div>
+    </section>
+)
+}
+
+export default MainVisual;
